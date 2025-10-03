f21329ed75eb78bc373f60097bf208a2492600a3
shankarbk <sbk.qubits@gmail.com>
2025-10-03 15:30:08 +0530

    corrections

--- infra/variables.tf
+++ infra/variables.tf
@@ -12,9 +12,13 @@ variable "instance_type" {
     default = "t3.small" 
 }
 
-variable "my_ip_cidr" { 
-    type = string
-    default = "0.0.0.0/0"
+variable "my_ip_cidr" {
+  type        = string
+  description = "Public IPv4 you SSH from, CIDR (e.g. 49.207.145.12/32)."
+  validation {
+    condition     = can(cidrnetmask(var.my_ip_cidr))
+    error_message = "my_ip_cidr must be a valid CIDR, like 203.0.113.10/32."
+  }
 }
 
 variable "sql_sa_password" { 
