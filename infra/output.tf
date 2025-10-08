0be767b1ff787a7b02f32a9379e572ac93a89c15
shankarbk <sbk.qubits@gmail.com>
2025-10-08 15:06:43 +0530

    sensitive removed

--- infra/output.tf
+++ infra/output.tf
@@ -1,34 +1,34 @@
 output "vpc_id" {
   value = data.aws_vpc.default.id
-    sensitive = false
+    
 }
 
 output "subnet_id" {
   value = local.selected_subnet_id
-    sensitive = false
+    
 }
 
 output "security_group_id" {
   value = aws_security_group.app.id
-    sensitive = false
+    
 }
 
 output "instance_id" {
   value = aws_instance.app.id
-    sensitive = false
+    
 }
 
 output "instance_public_ip" {
   value = aws_instance.app.public_ip
-    sensitive = false
+    
 }
 
 output "instance_public_dns" {
   value = aws_instance.app.public_dns
-    sensitive = false
+    
 }
 
 output "windows_ami_id" {
   value = data.aws_ssm_parameter.win2022_ami.value
-  sensitive = false
+  
 }
